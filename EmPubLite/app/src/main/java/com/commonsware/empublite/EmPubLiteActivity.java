0a4377f93a1c35a4499f0018a0b88c65cf00e8b9
dashingvampire <suryavanshrai13@gmail.com>
2020-07-19 19:32:08 +0530

    adding project

---
+++ EmPubLite/app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -0,0 +1,18 @@
+package com.commonsware.empublite;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class EmPubLiteActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_list_demo);
+  }
+
+  public void onClickButton(View view) {
+    // do something useful heresc
+  }
+}
