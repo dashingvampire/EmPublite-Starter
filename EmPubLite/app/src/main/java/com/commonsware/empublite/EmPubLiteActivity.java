72c742351a380023c72c25c1c08cf9fac0889ca5
dashingvampire <suryavanshrai13@gmail.com>
2020-07-20 05:01:20 +0530

    trying new ways to implement click listener.

--- EmPubLite/app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ EmPubLite/app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -4,21 +4,42 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class EmPubLiteActivity extends Activity {
+public class EmPubLiteActivity extends Activity implements View.OnClickListener {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
+
+    Button button0 = (Button) findViewById(R.id.button0);
+    Button button1 = (Button) findViewById(R.id.button1);
+    button0.setOnClickListener(this);
+    button1.setOnClickListener(this);
   }
 
-  public void onClickButton0(View view) {
+  public void onClick(View view){
+    switch(view.getId()){
+      case R.id.button0:
+        Intent intent0 = new Intent(this, ListActivityDemo.class);
+        startActivity(intent0);
+        break;
+      case R.id.button1:
+        Intent intent1 = new Intent(this, FormTableLayout.class);
+        startActivity(intent1);
+        break;
+    }
+  }
+
+  /*public void onClickButton0(View view) {
     Intent intent = new Intent(EmPubLiteActivity.this, ListActivityDemo.class);
     startActivity(intent);
   }
 
   public void onClickButton1(View view) {
+    Intent intent = new Intent(EmPubLiteActivity.this, FormTableLayout.class);
+    startActivity(intent);
   }
 
   public void onClickButton2(View view) {
@@ -31,5 +52,6 @@ public class EmPubLiteActivity extends Activity {
   }
 
   public void onClickButton5(View view) {
-  }
+  }*/
+
 }
---
+++ EmPubLite/app/src/main/java/com/commonsware/empublite/FormTableLayout.java
@@ -0,0 +1,13 @@
+package com.commonsware.empublite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormTableLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_table_layout);
+    }
+}
