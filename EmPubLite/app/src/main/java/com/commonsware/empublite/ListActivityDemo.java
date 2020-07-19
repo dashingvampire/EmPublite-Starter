40bd251499c00ef572d0fe15bf252e152ccd25ad
dashingvampire <suryavanshrai13@gmail.com>
2020-07-20 04:17:22 +0530

    just fixed the error. created multiple layouts with different buttons

--- EmPubLite/app/src/main/java/com/commonsware/empublite/ListActivityDemo.java
+++ EmPubLite/app/src/main/java/com/commonsware/empublite/ListActivityDemo.java
@@ -19,6 +19,7 @@ public class ListActivityDemo extends ListActivity {
             "etiam", "vel", "erat", "placerat", "ante",
             "porttitor", "sodales", "pellentesque", "augue", "purus"};
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
